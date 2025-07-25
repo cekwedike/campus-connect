d2b448218c36e955891b8362a01e8e04db3a226c
cekwedike <c.ekwedike@alustudent.com>
2025-07-26 02:09:15 +0300

    chore: Remove obsolete Terraform modules and related files
    - Deleted multiple Terraform modules including Azure Container Registry, Application Gateway, Container Apps, Database, VPC, and Virtual Network to streamline the infrastructure codebase.
    - This cleanup focuses on removing deprecated components and enhancing project maintainability.

--- terraform/modules/app_gateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_address" {
-  description = "Application Gateway public IP address"
-  value       = azurerm_public_ip.main.ip_address
-}
-
-output "gateway_url" {
-  description = "Application Gateway URL"
-  value       = "http://${azurerm_public_ip.main.ip_address}"
-} 
